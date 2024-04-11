7200af08b289f35b6ca6274476132e7009f70f12
Kaleb Cole <93733756+KalebCole@users.noreply.github.com>
2024-04-11 08:30:10 -0400

    added view functionality

--- Crewmates/src/App.jsx
+++ Crewmates/src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRoutes } from 'react-router-dom'
 import "./App.css";
 import { Link } from 'react-router-dom'
 import Home from './pages/Home'
 import EditCrew from './pages/EditCrew'
 import AddCrew from './pages/AddCrew'
+import ViewCrew from './pages/ViewCrew'
+import { supabase } from './client'
 
 function App() {
+
+
+  //getting the crew data from supabase
+  const [crewMembers, setCrewMembers] = useState([]);
+
+    useEffect(() => {
+        //call the supabase database to get the crew members
+        async function getCrewMembers() {
+            const {data} = await supabase.from("Crew").select().order('created_at', {ascending: true});
+            setCrewMembers(data);
+        }
+        getCrewMembers();
+    }, []);
 
   //set up routing
   const element = useRoutes([
@@ -16,19 +31,25 @@ function App() {
     },
     {
       path: "/edit/:id",
-      element: <EditCrew />,
+      element: <EditCrew crew={crewMembers}/>,
     },
     {
       path: "/new",
       element: <AddCrew />,
+    },
+    {
+      path: "/view",
+      element: <ViewCrew crew={crewMembers}/>,
     }
   ]);
+
 
   return <>
     <div className="header">
       <h1>👍 Crewmates 1.0</h1>
-      <Link to="/"><button className="headerBtn"> Explore Crewmates 🔍  </button></Link>
+      <Link to="/"><button className="headerBtn"> Home  </button></Link>
       <Link to="/new"><button className="headerBtn"> Add Crewmate 🏆 </button></Link>
+      <Link to="/view"><button className="headerBtn">Explore Crewmates 🔍</button></Link>
     </div>
     {element}
   
